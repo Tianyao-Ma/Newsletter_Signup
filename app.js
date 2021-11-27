39c8fd6e77934f3a1c3a3b50b4ef26d007c561c7
Tianyao Ma <tianyao.ma333@gmail.com>
2021-11-26 23:16:04 -0500

    setup signUp page and able to extract information from the form through post method

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ const request = require("request");
 
 const app = express();
 
+//specified the static file folder;
 
-app.get('/', function(req, res) {
-    res.send("Hello!")
-})
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+    let firstName = req.body.fName;
+    let lastName = req.body.lName;
+    let email = req.body.email;
+    console.log(firstName, lastName, email);
+});
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000");
-})
+});
