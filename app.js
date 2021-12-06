53cbc5e7221ebd2f08d8520969f61ab81142e27c
Tianyao Ma <tianyao.ma333@gmail.com>
2021-12-05 23:24:20 -0500

    adding success and failure page and re-direct from failure page to home page

--- app.js
+++ app.js
@@ -9,55 +9,59 @@ const app = express();
 //specified the static file folder;
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req, res) {
+app.post("/", function (req, res) {
     const firstName = req.body.fName;
     const lastName = req.body.lName;
     const email = req.body.email;
-    
+
     const data = {
         members: [
             {
-                email_address: email, 
+                email_address: email,
                 status: "subscribed",
                 merge_fields: {
                     FNAME: firstName,
                     LNAME: lastName
                 }
             }
-        ] 
+        ]
     };
 
     const jsonData = JSON.stringify(data);
-    const url =  "https://us20.api.mailchimp.com/3.0/lists/86e2d25a72";
+    const url = "https://us20.api.mailchimp.com/3.0/lists/86e2d25a72";
 
     const options = {
-        method: "POST", 
+        method: "POST",
         auth: "tm:ac63c745993f9cdeab8a4ee45f7b4953-us20"
     }
 
-    const request = https.request(url, options, function(response) {
-        response.on("data", function(data) {
-            console.log(JSON.parse(data));
-        })
-    })
+    const request = https.request(url, options, function (response) {
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
 
+        response.on("data", function (data) {
+            console.log(JSON.parse(data));
+        });
+        
+        // request.end();
+    });
     request.write(jsonData);
     request.end();
 });
 
-app.listen(3000, function() {
-    console.log("Server is running on port 3000");
+app.post("/failure", function(req, res) {
+    res.redirect("/");
 });
 
-
-// API key
-//ac63c745993f9cdeab8a4ee45f7b4953-us20
-
-// list id
-//86e2d25a72
+app.listen(3000, function () {
+    console.log("Server is running on port 3000");
+});
